0115b32a52629486eed606cf252cbf892609bcb2
cluebox <jinucluebox@gmail.com>
2019-07-16 09:56:40 +0530

    Final commit

---
+++ Maven-CoolApp/src/main/java/com/jinu/maven/Maven_CoolApp/App.java
@@ -0,0 +1,13 @@
+package com.jinu.maven.Maven_CoolApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World of meven quike start project.." );
+    }
+}
